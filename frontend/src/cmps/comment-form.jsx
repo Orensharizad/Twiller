a40c9fe274bfa053653ef7ea059004e9cee09f85
liad7 <liadgola7@gmail.com>
2023-02-05 14:25:23 +0200

    add required to comment form

--- frontend/src/cmps/comment-form.jsx
+++ frontend/src/cmps/comment-form.jsx
@@ -3,12 +3,13 @@ import { commentService } from "../services/comment.service"
 
 export function CommentForm({ setComments }) {
 
-    const [register, comment] = useFormRegister(commentService.getEmptyComment())
+    const [register, comment, setComment] = useFormRegister(commentService.getEmptyComment())
 
     const sendComment = async (ev, comment) => {
         ev.preventDefault()
         try {
             const newComment = await commentService.save(comment)
+            setComment(commentService.getEmptyComment())
             setComments(prevComments => [newComment, ...prevComments])
         } catch (err) {
             console.log('Cannot save comment:', err)
@@ -17,8 +18,8 @@ export function CommentForm({ setComments }) {
 
     return (
         <form className="comment-form" onSubmit={(ev) => sendComment(ev, comment)}>
-            <input placeholder="Email" {...register('email')} />
-            <textarea placeholder="Message" {...register('txt')}></textarea>
+            <input placeholder="Email" {...register('email')} required />
+            <textarea placeholder="Message" {...register('txt')} required></textarea>
             <button>Submit</button>
         </form>
     )
