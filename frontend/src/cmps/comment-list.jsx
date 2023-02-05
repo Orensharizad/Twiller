7d443d872fcbf2a8381c9d0bbb7fe59ad9d3d1fd
Oren Sharizad <oren.sharizad12@gmail.com>
2023-02-05 14:02:23 +0200

    added a list and preview cmps

---
+++ frontend/src/cmps/comment-list.jsx
@@ -0,0 +1,8 @@
+export function CommentList() {
+    return (
+        <section className='comment-list'>
+            hellofrom list
+
+        </section>
+    )
+}
--- frontend/src/pages/comment-index.jsx
+++ frontend/src/pages/comment-index.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react"
 import { CommentForm } from "../cmps/comment-form"
+import { CommentList } from "../cmps/comment-list"
+import { commentService } from "../services/comment.service"
 
 export function CommentIndex() {
+    const [comments, setComments] = useState([])
+
+    useEffect(() => {
+        loadComments()
+    }, [])
+
+    const loadComments = async () => {
+        try {
+            const comments = await commentService.query()
+            setComments(comments)
+
+        } catch (err) {
+            console.error(err, 'cannot get commets')
+        }
+
+    }
+
 
     return (
         <section className="comment-index">
             <CommentForm />
+            <CommentList />
         </section>
     )
 }
