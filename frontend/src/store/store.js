6b250b2d761f3178889c43c0bda6decb958d6023
liad7 <liadgola7@gmail.com>
2023-02-05 14:41:09 +0200

    basic scss file for cmps

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -8,10 +8,8 @@ const rootReducer = combineReducers({
     systemModule: systemReducer,
 })
 
-
 const middleware = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : undefined
 export const store = createStore(rootReducer, middleware)
-
 
 store.subscribe(() => {
     console.log('**** Store state changed: ****')
