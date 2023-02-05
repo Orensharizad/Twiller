d70a22cdb990913e8e6f8c21db9bd5b783a37a5e
liad7 <liadgola7@gmail.com>
2023-02-05 14:33:09 +0200

    cleaning code

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,15 +1,11 @@
 import { createStore, combineReducers } from 'redux'
 
-import { carReducer } from './car.reducer.js'
 import { userReducer } from './user.reducer.js'
-import { reviewReducer } from './review.reducer'
 import { systemReducer } from './system.reducer'
 
 const rootReducer = combineReducers({
-    carModule: carReducer,
     userModule: userReducer,
     systemModule: systemReducer,
-    reviewModule: reviewReducer,
 })
 
 
