33f57d561e78a876d26982bac480fba6509d658b
liad7 <liadgola7@gmail.com>
2023-02-05 16:37:25 +0200

    cleaning code

--- frontend/src/cmps/app-footer.jsx
+++ frontend/src/cmps/app-footer.jsx
@@ -3,13 +3,10 @@ import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 
-import { UserMsg } from './user-msg.jsx'
-
 export function AppFooter() {
 
     return (
         <footer className="app-footer">
-            <UserMsg />
         </footer>
     )
 }
--- frontend/src/root-cmp.jsx
+++ frontend/src/root-cmp.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router'
 
-import { AppHeader } from './cmps/app-header'
 import { AppFooter } from './cmps/app-footer'
 import { CommentIndex } from './pages/comment-index'
 import { MainSideMenu } from './cmps/main-side-menu'
@@ -9,7 +8,6 @@ import { MainSideMenu } from './cmps/main-side-menu'
 export function RootCmp() {
     return (
         <div>
-            {/* <AppHeader /> */}
             <main className="main-container">
                 <Routes>
                     <Route path="/comment" element={<CommentIndex />} />
